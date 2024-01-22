7a442821b7757243ff1645c28ccef3ea5cb0fbd8
Zeta201 <dini2040gh@gmail.com>
2024-01-22 20:57:13 +0530

    add translator module

--- apk/adapter/internal/operator/controllers/dp/gateway_api/model/translation/types.go
+++ apk/adapter/internal/operator/controllers/dp/gateway_api/model/translation/types.go
@@ -3,16 +3,14 @@ package translation
 import (
 	v1 "k8s.io/api/core/v1"
 
-	"github.com/wso2/apk/adapter/internal/operator/synchronizer"
+	"github.com/wso2/apk/adapter/internal/operator/controllers/dp/gateway_api/model"
 	ciliumv2 "github.com/wso2/apk/common-go-libs/apis/cilium.io/v2"
 )
-
-const ClusterTypeURL = "type.googleapis.com/envoy.config.cluster.v3.Cluster"
 
 // Translator is the interface to take the model and generate required CiliumEnvoyConfig,
 // LoadBalancer Service, Endpoint, etc.
 //
 // Different use cases (e.g. Ingress, Gateway API) can provide its own generation logic.
 type Translator interface {
-	Translate(gatewayState *synchronizer.APIState) (*ciliumv2.CiliumEnvoyConfig, *v1.Service, *v1.Endpoints, error)
+	Translate(model *model.Model) (*ciliumv2.CiliumEnvoyConfig, *v1.Service, *v1.Endpoints, error)
 }
